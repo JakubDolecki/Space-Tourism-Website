9a7f1cfdba31c4237fdbb23029e30b59cba84343
JakubDolecki <jakubdolecki1@wp.pl>
2023-10-12 13:42:03 +0200

    deploy

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./components/HomePage";
